4197f3e6593c61a52b0f19f2210963da7cf3939e
Ben Evans <ben.d.evans@gmail.com>
2016-08-21 15:38:02 +0100

    Remove unnecessary files

--- squash.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-#sudo docker run -d pyrho/minimal
-#sudo docker export pyrho/minimal | sudo docker import - pyrho/minimal:latest #squashed:new
-
-ID=$(docker run -d pyrho/minimal /bin/bash)
-docker export $ID | docker import – prometheus
